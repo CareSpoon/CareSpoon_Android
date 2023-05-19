c0cd90283e1e563d4875ac04efecac2adba7dc2a
hyuna <akxmcse@gmail.com>
2023-05-19 13:45:56 +0900

    [feat] 유저 검색 api, 친구 추가 api 연동

--- app/src/main/java/org/joyroom/carespoon/ui/friends/AddFriendActivity.kt
+++ app/src/main/java/org/joyroom/carespoon/ui/friends/AddFriendActivity.kt
@@ -1,36 +1,66 @@
 package org.joyroom.carespoon.ui.friends
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import org.joyroom.carespoon.data.CareSpoonSharedPreferences
 import org.joyroom.carespoon.data.local.FriendsData
 import org.joyroom.carespoon.data.local.UserData
 import org.joyroom.carespoon.databinding.ActivityAddFriendBinding
 import org.joyroom.carespoon.databinding.ActivityFriendsBinding
+import org.joyroom.carespoon.ui.viewModel.FriendsViewModel
 
 class AddFriendActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddFriendBinding
-    private lateinit var addFriendAdapter: AddFriendAdapter
+    private val viewModel: FriendsViewModel by viewModels()
+    private lateinit var friendUUID: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddFriendBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initAdapter()
+        initSearch()
+        initClickListener()
     }
 
-    private fun initAdapter(){
-        addFriendAdapter = AddFriendAdapter()
-        binding.rvUserList.adapter = addFriendAdapter
-
-        addFriendAdapter.userList.addAll(
-            listOf(
-                UserData("구미진", ""),
-                UserData("김현아", ""),
-                UserData("도소현", ""),
-            )
-        )
-
-        addFriendAdapter.notifyDataSetChanged()
+    private fun initSearch(){
+        binding.etSearch.setOnEditorActionListener { v, actionId, event ->
+            Log.d("***************Entered", " ")
+            var handled = false
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                val uuid = binding.etSearch.text
+                viewModel.requestUser(uuid)
+                setUserObserver()
+                handled = true
+            }
+            handled
+        }
     }
+    private fun setUserObserver(){
+        viewModel.userList.observe(this, Observer { user ->
+            binding.tvUserName.visibility = View.VISIBLE
+            binding.tvUserName.text = user.name
+            binding.button.visibility = View.VISIBLE
+            friendUUID = user.uuid
+        })
+    }
+
+    private fun initClickListener(){
+        binding.button.setOnClickListener{
+            addFriend()
+        }
+    }
+
+    private fun addFriend(){
+        val myRole = CareSpoonSharedPreferences.getUserRole()
+        val myUUID = CareSpoonSharedPreferences.getUUID()
+        if(myRole == "viewer") myUUID?.let { viewModel.requestAddFriend(it, friendUUID) }
+        else myUUID?.let { viewModel.requestAddFriend(friendUUID, it) }
+    }
+
 }
---
+++ app/src/main/java/org/joyroom/carespoon/ui/viewModel/FriendsViewModel.kt
@@ -0,0 +1,54 @@
+package org.joyroom.carespoon.ui.viewModel
+
+import android.app.Application
+import android.text.Editable
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.joyroom.carespoon.data.api.RetrofitBuilder
+import org.joyroom.carespoon.data.remote.request.friend.RequestAddFriend
+import org.joyroom.carespoon.data.remote.response.friend.ResponseSearchUser
+import org.joyroom.carespoon.data.remote.response.friend.ResponseSeniorFriendList
+import org.joyroom.carespoon.data.remote.response.friend.ResponseViewerFriendList
+
+class FriendsViewModel(application: Application) : AndroidViewModel(application) {
+    private val _seniorFriendList = MutableLiveData<ResponseSeniorFriendList>()
+    val seniorFriendList: LiveData<ResponseSeniorFriendList>
+        get() = _seniorFriendList
+
+    private val _viewerFriendList = MutableLiveData<ResponseViewerFriendList>()
+    val viewerFriendList: LiveData<ResponseViewerFriendList>
+        get() = _viewerFriendList
+
+    private val _userList = MutableLiveData<ResponseSearchUser>()
+    val userList : LiveData<ResponseSearchUser>
+        get() = _userList
+
+
+    fun requestSeniorFriendList(uuid: String) = viewModelScope.launch(Dispatchers.IO) {
+        _seniorFriendList.postValue(
+            RetrofitBuilder.friendService.getSeniorFriendList(uuid)
+        )
+    }
+
+    fun requestViewerFriendList(uuid: String) = viewModelScope.launch(Dispatchers.IO) {
+        _viewerFriendList.postValue(
+            RetrofitBuilder.friendService.getViewerFriendList(uuid)
+        )
+    }
+
+    fun requestUser(uuid: Editable) = viewModelScope.launch(Dispatchers.IO) {
+        _userList.postValue(
+            RetrofitBuilder.friendService.searchUser(uuid.toString())
+        )
+    }
+
+    fun requestAddFriend(viewerUUID: String, seniorUUID: String) = viewModelScope.launch(Dispatchers.IO){
+        RetrofitBuilder.friendService.addFriend(
+            RequestAddFriend(viewerUUID, seniorUUID)
+        )
+    }
+}
