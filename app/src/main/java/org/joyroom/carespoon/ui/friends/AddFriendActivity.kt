e1317a79c76a3440efe7f7ed6dd35d36a569ed6d
hyuna <akxmcse@gmail.com>
2023-05-22 04:19:42 +0900

    [feat] editText키보드 제어 관련 코드 추가

--- app/src/main/java/org/joyroom/carespoon/ui/friends/AddFriendActivity.kt
+++ app/src/main/java/org/joyroom/carespoon/ui/friends/AddFriendActivity.kt
@@ -1,12 +1,16 @@
 package org.joyroom.carespoon.ui.friends
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import org.joyroom.carespoon.R
 import org.joyroom.carespoon.data.CareSpoonSharedPreferences
 import org.joyroom.carespoon.data.local.FriendsData
 import org.joyroom.carespoon.data.local.UserData
@@ -30,29 +34,42 @@ class AddFriendActivity : AppCompatActivity() {
 
     private fun initSearch(){
         binding.etSearch.setOnEditorActionListener { v, actionId, event ->
-            Log.d("***************Entered", " ")
             var handled = false
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 val uuid = binding.etSearch.text
-                viewModel.requestUser(uuid)
-                setUserObserver()
+                if(uuid.contains(".")) {
+                    Toast.makeText(this, R.string.do_not_contain_dot, Toast.LENGTH_SHORT).show()
+                } else {
+                    viewModel.searchUser(uuid)
+                    setUserObserver()
+                }
+
+                // 키보드 내리기
+                val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(binding.etSearch.windowToken, 0)
                 handled = true
             }
             handled
         }
     }
+
     private fun setUserObserver(){
         viewModel.userList.observe(this, Observer { user ->
-            binding.tvUserName.visibility = View.VISIBLE
-            binding.tvUserName.text = user.name
-            binding.button.visibility = View.VISIBLE
-            friendUUID = user.uuid
+            if(user == null){
+                binding.tvUserNull.visibility = View.VISIBLE
+            }else{
+                binding.tvUserName.visibility = View.VISIBLE
+                binding.tvUserName.text = user.name
+                binding.button.visibility = View.VISIBLE
+                friendUUID = user.uuid
+            }
         })
     }
 
     private fun initClickListener(){
         binding.button.setOnClickListener{
             addFriend()
+            Toast.makeText(this, R.string.add, Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -62,5 +79,4 @@ class AddFriendActivity : AppCompatActivity() {
         if(myRole == "viewer") myUUID?.let { viewModel.requestAddFriend(it, friendUUID) }
         else myUUID?.let { viewModel.requestAddFriend(friendUUID, it) }
     }
-
 }
