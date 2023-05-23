1ae977d1a018d1635b6e5241f2bc3e623a076a7d
hyuna <akxmcse@gmail.com>
2023-05-23 18:17:46 +0900

    [feat] camerax 이용해 multipart로 이미지 넘기기 구현

--- app/src/main/java/org/joyroom/carespoon/ui/CameraActivity.kt
+++ app/src/main/java/org/joyroom/carespoon/ui/CameraActivity.kt
@@ -1,19 +1,36 @@
 package org.joyroom.carespoon.ui
 
 import android.annotation.SuppressLint
+import android.content.ContentUris
+import android.content.Context
+import android.content.pm.PackageManager
+import android.database.Cursor
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.DocumentsContract
+import android.provider.MediaStore
 import android.util.Log
+import android.util.Size
 import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import org.joyroom.carespoon.R
+import org.joyroom.carespoon.data.CareSpoonSharedPreferences
 import org.joyroom.carespoon.databinding.ActivityCameraBinding
+import org.joyroom.carespoon.ui.viewModel.MealViewModel
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.concurrent.ExecutorService
@@ -21,29 +38,63 @@ import java.util.concurrent.Executors
 
 class CameraActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCameraBinding
+    private val viewModel: MealViewModel by viewModels()
 
-    private var preview : Preview? = null
+    private var preview: Preview? = null
     private var imageCapture: ImageCapture? = null
 
     private lateinit var outputDirectory: File
     private lateinit var cameraExecutor: ExecutorService
+    private lateinit var savedUri: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCameraBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        startCamera()
-        binding.ivTakeBtn.setOnClickListener{
-            takePhoto()
-            val msg = "촬영이 완료되었습니다!"
-            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-        }
+        permissionCheck()
+
         outputDirectory = getOutputDirectory()
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
-    private fun startCamera(){
+    private fun permissionCheck() {
+        val cameraPermissionCheck = ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.CAMERA
+        )
+        if (cameraPermissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.CAMERA),
+                1000
+            )
+        } else {
+            startCamera()
+            setArea()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1000) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) { //거부
+                Toast.makeText(this, "카메라 접근이 허용되지 않았습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun setArea() {
+        binding.ivTakeBtn.setOnClickListener {
+            takePhoto()
+        }
+    }
+
+    private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
         cameraProviderFuture.addListener({
@@ -57,8 +108,8 @@ class CameraActivity : AppCompatActivity() {
                     it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
                 }
 
-            // ImageCapture
             imageCapture = ImageCapture.Builder().build()
+            // imageCapture = ImageCapture.Builder().setTargetResolution(Size(150, 200)).build() -> 해상도 조절
 
             // Select back camera as a default
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
@@ -72,9 +123,10 @@ class CameraActivity : AppCompatActivity() {
                     this,
                     cameraSelector,
                     preview,
-                    imageCapture)
+                    imageCapture
+                )
 
-            } catch(exc: Exception) {
+            } catch (exc: Exception) {
                 Log.d("CameraX-Debug", "Use case binding failed", exc)
             }
 
@@ -88,7 +140,8 @@ class CameraActivity : AppCompatActivity() {
         // Create time-stamped output file to hold the image
         val photoFile = File(
             outputDirectory,
-            newJpgFileName())
+            newJpgFileName()
+        )
 
         // Create output options object which contains file + metadata
         val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
@@ -104,16 +157,28 @@ class CameraActivity : AppCompatActivity() {
                 }
 
                 override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                    val savedUri = Uri.fromFile(photoFile)
-                    val msg = "Photo capture succeeded: $savedUri"
-                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                    Log.d("CameraX-Debug", msg)
+                    savedUri = Uri.fromFile(photoFile)
+
+                    val file = File(savedUri.path!!)
+                    val requestFile: RequestBody =
+                        RequestBody.create("multipart/form-data".toMediaTypeOrNull(), file)
+                    val image = MultipartBody.Part.createFormData("image", file.name, requestFile)
+                    val uuid = CareSpoonSharedPreferences.getUUID().toString()
+                    val tag = intent.getStringExtra("tag").toString()
+
+                    viewModel.requestUploadPhoto(image, uuid, tag)
+
+                    viewModel.showErrorToast.observe(this@CameraActivity, Observer {
+                        it.getContentIfNotHandled()?.let{
+                            Toast.makeText(baseContext, R.string.move, Toast.LENGTH_SHORT).show()
+                        }
+                    })
                 }
             })
     }
 
     @SuppressLint("SimpleDateFormat")
-    private fun newJpgFileName() : String {
+    private fun newJpgFileName(): String {
         val sdf = SimpleDateFormat("yyyyMMdd_HHmmss")
         val filename = sdf.format(System.currentTimeMillis())
         return "${filename}.jpg"
@@ -132,5 +197,6 @@ class CameraActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         cameraExecutor.shutdown()
+        //uploadPhoto()
     }
 }
